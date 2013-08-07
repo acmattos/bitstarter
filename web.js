8c197ee234a520155fdfc6e955f9708b695d0438
Andre Mattos <acmattos@gmail.com>
2013-08-07 02:17:15 +0000

    Changes due to hw6

--- web.js
+++ web.js
@@ -9,7 +9,17 @@ app.get('/', function(request, response) {
   response.send(buffer.toString());
 });
 
-var port = process.env.PORT || 5000;
+app.get('/css/bootstrap.min.css', function(request, response) {
+   var buffer = fs.readFileSync("css/bootstrap.min.css");
+   response.send(buffer.toString());
+});
+
+app.get('/js/bootstrap.min.js', function(request, response) {
+   var buffer = fs.readFileSync("js/bootstrap.min.js");
+   response.send(buffer.toString());
+});
+
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
